5f90ca8b86ec7ea7569bfb261b80d2b531778928
Crowdbotics <team@crowdbotics.com>
2021-09-04 05:49:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quiet_tree_30310Identifier",
+  url: "https://quiet_tree_30310Identifier.botics.co",
+  api: "https://quiet_tree_30310Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
